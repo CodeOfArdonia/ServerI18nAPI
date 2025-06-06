e2d59e315f3222c1d10c3b4bf12f365299c6b513
IAFEnvoy <tanksheng545@126.com>
2025-06-06 18:07:00 +0800

    to 1.20.6

--- neoforge/src/main/java/com/iafenvoy/server/i18n/neoforge/ServerI18nApiNeoForge.java
+++ neoforge/src/main/java/com/iafenvoy/server/i18n/neoforge/ServerI18nApiNeoForge.java
@@ -3,11 +3,12 @@ package com.iafenvoy.server.i18n.neoforge;
 import com.iafenvoy.server.i18n.ServerI18nApi;
 import com.iafenvoy.server.i18n.ServerI18nReloader;
 import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.event.AddReloadListenerEvent;
 
 @Mod(ServerI18nApi.MOD_ID)
-@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE)
+@EventBusSubscriber(bus = EventBusSubscriber.Bus.GAME)
 public final class ServerI18nApiNeoForge {
     public ServerI18nApiNeoForge() {
     }
