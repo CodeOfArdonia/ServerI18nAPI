eae25d2bc5214b87d53536fa254de17ac6c75e57
Teddy Li <teddyxlandlee@hotmail.com>
2026-06-25 12:52:40 +0800

    Optimize `ServerI18nReloader`

--- src/main/java/com/iafenvoy/server/i18n/ServerI18nReloader.java
+++ src/main/java/com/iafenvoy/server/i18n/ServerI18nReloader.java
@@ -8,6 +8,8 @@ import net.minecraft.server.packs.resources.Resource;
 import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.server.packs.resources.ResourceManagerReloadListener;
 
+import java.io.Reader;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -15,32 +17,51 @@ public enum ServerI18nReloader implements ResourceManagerReloadListener {
     INSTANCE;
     public static final ResourceLocation ID = /*? >=1.21 {*/ResourceLocation.fromNamespaceAndPath/*?} else {*//*new ResourceLocation*//*?}*/(ServerI18nApi.MOD_ID, ServerI18nApi.MOD_ID);
     public static final String DEFAULT_LANGUAGE = "en_us";
-    private static final Map<String, Map<String, String>> DATA = new HashMap<>();
+    private static volatile Map<String, Map<String, String>> DATA = Collections.emptyMap();
 
     @Override
     public void onResourceManagerReload(ResourceManager manager) {
-        DATA.clear();
+        final Map<String, Map<String, String>> pendingData = new HashMap<>();
         for (Map.Entry<ResourceLocation, Resource> entry : manager.listResources("lang", p -> p.getPath().endsWith(".json")).entrySet()) {
-            String language = entry.getKey().getPath().replaceAll(".json", "").replaceAll("lang/", "");
-            if (!DATA.containsKey(language)) DATA.put(language, new HashMap<>());
-            Map<String, String> map = DATA.get(language);
+            String language = removeSurrounding(entry.getKey().getPath());
+
+            Map<String, String> map = pendingData.computeIfAbsent(language, k -> new HashMap<>());
             try {
-                JsonElement element = JsonParser.parseReader(entry.getValue().openAsReader());
+                JsonElement element;
+                try (Reader reader = entry.getValue().openAsReader()) {
+                    element = JsonParser.parseReader(reader);
+                }
                 if (!element.isJsonObject()) continue;
                 JsonObject obj = element.getAsJsonObject();
-                for (String s : obj.keySet()) map.put(s, obj.get(s).getAsString());
+                obj.asMap().forEach((key, value) -> {
+                    if (!value.isJsonPrimitive()) return;
+                    map.put(key, value.getAsString());
+                });
             } catch (Exception e) {
                 ServerI18nApi.LOGGER.error("Failed to load {}", entry.getKey().toString(), e);
             }
         }
+        DATA = Collections.unmodifiableMap(pendingData);
+    }
+
+    private static String removeSurrounding(String s) {
+        int startIndex = 0, endIndex = s.length();
+        if (s.startsWith("lang/")) startIndex = "lang/".length();
+        if (s.endsWith(".json")) endIndex -= ".json".length();
+
+        if (startIndex > endIndex) return "";   // does not happen
+        return s.substring(startIndex, endIndex);
     }
 
     public static String translate(String language, String key) {
-        if (DATA.containsKey(language)) {
-            Map<String, String> map = DATA.get(language);
-            if (map.containsKey(key)) return DATA.get(language).get(key);
+        String value = null;
+        Map<String, String> map;
+        if ((map = DATA.get(language)) != null) {
+            value = map.get(key);
         }
-        if (DATA.containsKey(DEFAULT_LANGUAGE)) return DATA.get(DEFAULT_LANGUAGE).getOrDefault(key, key);
-        return key;
+        if (value == null && (map = DATA.get(DEFAULT_LANGUAGE)) != null) {
+            value = map.get(key);
+        }
+        return value != null ? value : key;
     }
 }
